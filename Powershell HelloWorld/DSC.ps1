6dad87c63b53d07789a63050a8c6e9967696f70a
Raji Subramanian <mail2raji@hotmail.com>
2018-08-23 15:42:16 -0500

    Powershell First Project

---
+++ Powershell HelloWorld/DSC.ps1
@@ -0,0 +1,18 @@
+﻿get-command *session*
+Get-PSSession
+Get-Command -Module PSDesiredStateConfiguration
+Get-WindowsFeature
+$env:PSModulePath -split ";"
+Get-DscResource
+Get-DscResource Windowsfeature | select *
+Get-DscResource Windowsfeature | Select -ExpandProperty Properties | Format-Table -AutoSize
+Get-DscResource Windowsfeature -Syntax
+Get-DscResource file -Syntax
+
+Snippet Ctrl + J
+Get-Command -CommandType Configuration
+psEdit C:\script\Script.ps1
+$PSHOME
+Get-DscConfiguration
+Start-DscConfiguration -Verbose
+Dir \\web01\test
